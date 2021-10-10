cd2527677c65214a6db658d171cf18656b8eb639
Colby M. White <colbywhite@users.noreply.github.com>
2021-10-10 12:37:28 -0500

    feat: add header; hardcode body

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,59 @@
 <script context="module" lang="ts">
-	export const prerender = true;
+  export const prerender = true;
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
 </script>
 
-<svelte:head>
-	<title>Home</title>
-</svelte:head>
+<h1>Rap Quotes</h1>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<figure>
+  <blockquote>
+    <p>
+      Y'all wearing dresses and purses, dressing in Birkins, fam. I put a cap in that gown - pop and
+      circumstance.
+    </p>
+  </blockquote>
+  <img src="https://i.scdn.co/image/ab67616d0000b27357641c5581a8d6527741748a" />
+  <figcaption>
+    <p>Joell Ortiz</p>
+    <cite><a href="https://open.spotify.com/album/5kCNU9ouaRoRtQNJrtfavl" target="_blank">Black Rock</a></cite>
+  </figcaption>
+</figure>
+<figure>
+  <blockquote>
+    <p>
+      The boy's slick like three kids stacked in an overcoat at a twenty-one and over show.
+    </p>
+  </blockquote>
+  <img src="https://i.scdn.co/image/ab67616d0000b273b7ddccc528879dbacb7662bb" />
+  <figcaption>
+    <p>Aesop Rock</p>
+    <cite><a href="https://open.spotify.com/album/3KJsSVL5vtJlb11tLHjdQN" target="_blank">Lice Team,
+      Baby</a></cite>
+  </figcaption>
+</figure>
+<figure>
+  <blockquote>
+    <p>
+      From the get-go, this pop can cause a panic at the disco.
+    </p>
+  </blockquote>
+  <img src="https://i.scdn.co/image/ab67616d0000b2739b7da1018417380cf15e54b9" />
+  <figcaption>
+    <p>Method Man</p>
+    <cite><a href="https://open.spotify.com/album/7q3ff38RxgNN0HbG5WNDWF" target="_blank">Hard Living</a></cite>
+  </figcaption>
+</figure>
+<figure>
+  <blockquote>
+    <p>
+      You give me steps to climb, I redesign. I made some bleachers.
+    </p>
+  </blockquote>
+  <img src="https://i.scdn.co/image/ab67616d0000b27357641c5581a8d6527741748a" />
+  <figcaption>
+    <p>Oddisee</p>
+    <cite><a href="https://open.spotify.com/album/5kCNU9ouaRoRtQNJrtfavl" target="_blank">No Trouble</a></cite>
+  </figcaption>
+</figure>
