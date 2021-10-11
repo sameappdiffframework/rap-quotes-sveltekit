da0be483f7a48ec4a44ff1cbf3e319391b395acb
Colby M. White <colbywhite@users.noreply.github.com>
2021-10-11 14:01:04 -0500

    feat: add footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,44 @@
+<footer>
+  <ul className="no-bullets">
+    <li>
+      <a href="https://github.com/sameappdiffframework" target="_blank" rel="noreferrer">
+        Same App, Different Framework
+      </a>
+    </li>
+    <li>
+      <a href="https://github.com/sameappdiffframework/rap-quotes-angular" target="_blank"
+         rel="noreferrer">
+        Source code
+      </a>
+    </li>
+  </ul>
+  <p>
+    This site is made by
+    <a href="https://github.com/colbywhite" target="_blank" rel="noreferrer">
+      Colby M. White
+    </a>
+    and hosted for free by Netlify.
+    It is a part of the
+    <a href="https://github.com/sameappdiffframework" target="_blank" rel="noreferrer">
+      Same App, Different Framework
+    </a>
+    project.
+  </p>
+</footer>
+
+<style>
+    footer {
+        font-size: .7em;
+        display: flex;
+        justify-content: space-between;
+        margin: 1em 0 0 0;
+        border-top: 1px solid var(--primary-accent);
+        padding: 1em 0 0 0;
+        gap: 1em;
+    }
+
+    footer p {
+        flex-basis: 50%;
+        margin: 0;
+    }
+</style>
