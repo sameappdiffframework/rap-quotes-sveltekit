d81958d3d3a23b7cebc6baab1e892119ed040c37
Colby M. White <colbywhite@users.noreply.github.com>
2021-10-17 17:03:14 -0500

    refactor: use lukeed/uuid

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -3,11 +3,11 @@
   import Header from "$lib/Header.svelte";
   import ModalContainer from "$lib/modal/ModalContainer.svelte";
   import CreateQuoteForm from "$lib/quotes/CreateQuoteForm.svelte";
-  import { quoteStore } from "$lib/quotes/quote-store";
   import type { QuoteFormInput } from "$lib/quotes/quote-form.utils";
   import type { QuoteModel } from "$lib/quotes/quote-store";
-  import { v4 as uuid } from 'uuid';
-  
+  import { quoteStore } from "$lib/quotes/quote-store";
+  import { v4 as uuid } from "@lukeed/uuid";
+
   let isModalOpen: boolean = false;
 
   function showModal() {
@@ -25,7 +25,7 @@
       artist: { name: input.artist },
       source: { name: input.source }
     };
-    quoteStore.update((oldQuotes => [quote, ...oldQuotes]));
+    quoteStore.update(oldQuotes => [quote, ...oldQuotes]);
     hideModal();
   }
 </script>
